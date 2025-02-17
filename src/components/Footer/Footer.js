e6d28ee49400dcbc6ce2b06a3fa70f22172fd5ed
Petter J. Ytterdahl <petterjy@hotmail.no>
2025-02-17 13:14:16 +0100

    Fixed BrowserRouter to navigate correct files.
    Fixed the BrowserRouter to navigate files properly.
    Implemented the HTML for the footer, which was in the BookingPage.html
    file, into the correct Footer.js file.
    Fixed the Header by removing redundant wrapper which misaligned the
    elements in the Header, when it was added to its own js file.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,32 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div></div>
-    </footer>
+    <footer>
+		<section id="contact">
+			<h3>Contact</h3>
+			<address>
+				<ul class="footer-item">
+					<li><a href="mailto:Ålesund@rental.com">Ålesund@rental.com</a></li>
+				</ul>
+			</address>
+		</section>
+		
+		<nav id="links">
+			<h3>Links</h3>
+			<ul class="footer-item">
+				<li><a href="https://github.com/">GitHub</a></li>
+			</ul>
+		</nav>
+		
+		<section id="creators">
+			<h3>Creators</h3>
+			<ul class="footer-item">
+				<li>Mathias Løvnes</li>
+				<li>Marcus Skaue</li>
+				<li>Petter Ytterdahl</li>
+			</ul>
+		</section>
+	</footer>
   );
 };
 
