e60405c330609e30dcc4ba010f37d3ac2e6ceb1e
Petter J. Ytterdahl <petterjy@hotmail.no>
2025-02-17 11:22:42 +0100

    Added react-router-dom to the App.js.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements, Navigate, Outlet} from "react-router-dom";
 import RentalPage from './RentalPage/RentalPage';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import CarDisplay from './CarDisplay/CarDisplay';
 import './App.css';
 
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path={"/"} element={ <Root />}>
+    <Route index element={<Navigate to={"/home"} replace />} />
+    <Route path={"home"} element={<Home />} />
+    <Route path={"about"} element={<About />} />
+    <Route path={"rental"} element={<RentalPage />} />
+    <Route path={"contact"} element={<Contact />} />
+  </Route>
+))
+
 function App() {
+return (
+    <RouterProvider router={router}/>
+);
+}
+
+function Root() {
   return (
     <div className="App">
       <Header />
